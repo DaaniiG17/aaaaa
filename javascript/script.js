7bb8fba4ff3944191dcc5c77382e29754c15d964
DaaniiG17 <168530762+DaaniiG17@users.noreply.github.com>
2024-05-11 18:20:35 +0200

    actualizacion

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,15 @@
-const eliminar = document.querySelectorAll('.eliminar');
+const deleteButtons = document.querySelectorAll('.delete-btn');
 
-eliminar.forEach(eliminar =>{
-    eliminar.addEventListener('click', eliminarJugador)
-})
+  // Iterar sobre cada botón y agregar un evento de clic
+  deleteButtons.forEach(button => {
+    button.addEventListener('click', function(event) {
+      // Prevenir el comportamiento predeterminado del enlace
+      event.preventDefault();
+      
+      // Obtener el contenedor del jugador (el div con clase "col-lg-4")
+      const playerContainer = this.parentElement.parentElement;
 
-function eliminarJugador(e){
-    e.preventDefault();
-    const jugador = e.target.parentNode.parentNode;
-    jugador.remove();
-}
+      // Eliminar el contenedor del jugador del DOM
+      playerContainer.remove();
+    });
+  });
