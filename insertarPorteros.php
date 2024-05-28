35ede5f2ee1331285a5f594432d7a8869560a3a1
DaaniiG17 <168530762+DaaniiG17@users.noreply.github.com>
2024-05-28 20:43:10 +0200

    a

--- insertarPorteros.php
+++ insertarPorteros.php
@@ -2,30 +2,37 @@
 // Incluir el archivo de conexión a la base de datos
 include 'phps/database.php';
 
-// Consulta SQL para seleccionar los porteros
-$sql = "SELECT * FROM alaves WHERE posicion = 'Portero'";
+// Verificar si se ha seleccionado un equipo
+if(isset($_GET['id'])) {
+    $equipo = $_GET['id'];
 
-// Ejecutar la consulta
-$resultado = $conn->query($sql);
+    // Consulta SQL para seleccionar los jugadores del equipo seleccionado
+    $sql = "SELECT * FROM $equipo WHERE posicion = 'Portero'";
 
-// Verificar si se encontraron porteros
-if ($resultado && $resultado->rowCount() > 0) {
-    // Mostrar los porteros encontrados
-    echo '<div class="container marketing">';
-    echo '<h1 class="mt-5">PORTEROS</h1>';
-    echo '<hr class="mb-5">';
-    echo '<div class="row text-center">';
-    while ($fila = $resultado->fetch(PDO::FETCH_ASSOC)) {
-        echo '<div class="col-lg-4 mb-5">';
-        echo '<img class="rounded-circle" width="140" height="140" src="' . $fila['imagen'] . '">';
-        echo '<h2>' . $fila['nombre'] . '</h2>';
-        echo '<h5>' . $fila['posicion'] . '</h5>';
-        echo '<button class="btn btn-secondary bg-primary eliminarPorteros">Eliminar jugador</button>';
-        echo '</div><!-- /.col-lg-4 -->';
+    // Ejecutar la consulta
+    $resultado = $conn->query($sql);
+
+    // Verificar si se encontraron jugadores
+    if ($resultado && $resultado->rowCount() > 0) {
+        // Mostrar los jugadores encontrados
+        echo '<div class="container marketing">';
+        echo '<h1 class="mt-5">PORTEROS</h1>';
+        echo '<hr class="mb-5">';
+        echo '<div class="row text-center">';
+        while ($fila = $resultado->fetch(PDO::FETCH_ASSOC)) {
+            echo '<div class="col-lg-4 mb-5">';
+            echo '<img class="rounded-circle" width="140" height="140" src="' . $fila['imagen'] . '">';
+            echo '<h2>' . $fila['nombre'] . '</h2>';
+            echo '<h5>' . $fila['posicion'] . '</h5>';
+            echo '<button class="btn btn-secondary bg-primary eliminarPorteros">Eliminar jugador</button>';
+            echo '</div><!-- /.col-lg-4 -->';
+        }
+        echo '</div><!-- /.row -->';
+        echo '</div>';
+    } else {
+        echo '<div class="container">No se encontraron porteros en el equipo seleccionado.</div>';
     }
-    echo '</div><!-- /.row -->';
-    echo '</div>';
 } else {
-    echo "No se encontraron porteros en el equipo.";
+    echo "No se ha seleccionado un equipo.";
 }
 ?>
