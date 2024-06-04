7df3c66f8d14a108bb08fab6b825c62d6a516131
DaaniiG17 <168530762+DaaniiG17@users.noreply.github.com>
2024-06-04 20:38:55 +0200

    a

--- phps/signup.php
+++ phps/signup.php
@@ -1,46 +1,53 @@
 <?php
 
-  require 'database.php';
+require 'database.php';
 
-  $message = '';
+$message = '';
 
-  if (!empty($_POST['email']) && !empty($_POST['password'])) {
-    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':email', $_POST['email']);
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-    $stmt->bindParam(':password', $password);
-
-    if ($stmt->execute()) {
-      $message = 'Usuario creado correctamente';
+if (!empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['confirm_password'])) {
+    // Verificar si las contraseñas coinciden
+    if ($_POST['password'] !== $_POST['confirm_password']) {
+        $message = 'Las contraseñas no coinciden.';
     } else {
-      $message = 'Lo siento, hubo un problema al crear su cuenta';
+        $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':email', $_POST['email']);
+        $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+        $stmt->bindParam(':password', $password);
+
+        if ($stmt->execute()) {
+            $message = 'Usuario creado correctamente';
+        } else {
+            $message = 'Lo siento, hubo un problema al crear su cuenta';
+        }
     }
-  }
+} else {
+    $message = 'Por favor, complete todos los campos.';
+}
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
     <title>SignUp</title>
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/styles.css">
-  </head>
-  <body>
+</head>
+<body>
 
-    <?php if(!empty($message)): ?>
-      <p> <?= $message ?></p>
-    <?php endif; ?>
+<?php if (!empty($message)): ?>
+    <p><?= $message ?></p>
+<?php endif; ?>
 
-    <h1>Registrate</h1>
-    <span>o <a href="login.php">inicia sesion</a></span>
+<h1>Registrate</h1>
+<span>o <a href="login.php">inicia sesion</a></span>
 
-    <form action="signup.php" method="POST">
-      <input name="email" type="text" placeholder="Introduce tu email">
-      <input name="password" type="password" placeholder="Introduce tu contraseña">
-      <input name="confirm_password" type="password" placeholder="Introduce de nuevo tu contraseña">
-      <input type="submit" value="Submit">
-    </form>
+<form action="signup.php" method="POST">
+    <input name="email" type="text" placeholder="Introduce tu email">
+    <input name="password" type="password" placeholder="Introduce tu contraseña">
+    <input name="confirm_password" type="password" placeholder="Introduce de nuevo tu contraseña">
+    <input type="submit" value="Submit">
+</form>
 
-  </body>
+</body>
 </html>
